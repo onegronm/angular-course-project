6dc6392fc2a182467ecc9470ccc1a8d40fa061f9
Omar Negron <omarnegron92@hotmail.com>
2021-03-23 23:37:03 -0400

    added recipe components

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header', // prefix components with the 'app' key so we don't conflict with HTML elements
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+}
