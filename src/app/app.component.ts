5c2fe4c73d4238ecb75ec553e631c2da7e87c0e3
Omar Negron <omarnegron92@hotmail.com>
2021-03-31 21:42:02 -0400

    navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  featureLoaded:string;
+
+  onFeatureSelected(feature){
+    this.featureLoaded = feature;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: 'app-header', // prefix components with the 'app' key so we don't conflict with HTML elements
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output() featureSelected = new EventEmitter<string>();
+
+    onNavigated(feature){
+        this.featureSelected.emit(feature);
+    }
 
 }
